f49c817299401bfcd39b122eef17639f52c55a87
unknown <annettabel@rambler.ru>
2024-01-07 15:47:10 +0500

    add get location user and context with location

---
+++ app/(context)/UserLocationContext.ts
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+export const UserLocationContext = createContext({
+  latitude: 52.244,
+  longitude: 26.804,
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Booking from "./(components)/Booking/Booking";
 import MapBoxMap from "./(components)/Map/MapBoxMap";
 import styles from "./Home.module.scss";
+import { UserLocationContext } from "./(context)/UserLocationContext";
 
 export default function Home() {
+  const [userLocation, setUserLocation] = useState({
+    latitude: 52.244,
+    longitude: 26.804,
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      console.log(pos);
+      setUserLocation({
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
+      });
+    });
+  }, []);
+
   return (
     <div className={styles.home}>
-      <div className={styles.container}>
+      <UserLocationContext.Provider value={userLocation}>
+        <div className={styles.container}>
           <Booking />
-        <div className="col-span-2">
-          <MapBoxMap/>
+          <div className="col-span-2">
+            <MapBoxMap />
+          </div>
         </div>
-      </div>
+      </UserLocationContext.Provider>
     </div>
   );
 }
